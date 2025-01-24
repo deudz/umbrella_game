4a2c4a070a033efebb35c5fc903e110d636b68e8
Danilo Soares <deudzdev@gmail.com>
2025-01-24 02:16:05 -0300

    add 32bit support

--- flake.nix
+++ flake.nix
@@ -7,17 +7,29 @@
 
   outputs = { self, nixpkgs }:
   let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs32 = nixpkgs.legacyPackages.i686-linux;
   in
   {
-    devShell.${system} = pkgs.mkShell {
+    devShells.x86_64-linux.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         gnumake
 	cmake
       ];
 
       buildInputs = with pkgs; [
+        imgui
+	SDL2
+      ];
+    };
+
+    devShells.i686-linux.default = pkgs32.mkShell {
+      nativeBuildInputs = with pkgs32; [
+        gnumake
+	cmake
+      ];
+
+      buildInputs = with pkgs32; [
         imgui
 	SDL2
       ];
